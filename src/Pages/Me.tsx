f35abf7bb2e56dd47d1e10ca1d7e5b0515d27d61
Juhász Csenge <csenge799@gmail.com>
2023-05-24 12:22:32 +0200

    js element

--- src/Pages/Me.tsx
+++ src/Pages/Me.tsx
@@ -8,44 +8,44 @@ import GifProfile from '../Components/GifProfile';
 
 export default function Me() {
 
-//  const [loopNum, setLoopNum] = useState(0);
-//  const [isDeleting, setIsDeleting] = useState(false);
-//  const toRotate = ["Web Developer", "Web Designer", "UI/UX Ddesigner"];
-//  const [text, setText] = useState("");
-//  const [delta, setDelta] = useState(300- Math.random() * 100);
-//  const period = 2000;
+ const [loopNum, setLoopNum] = useState(0);
+ const [isDeleting, setIsDeleting] = useState(false);
+ const toRotate = ["Web Developer", "Web Designer", "UI/UX Ddesigner"];
+ const [text, setText] = useState("");
+ const [delta, setDelta] = useState(300- Math.random() * 100);
+ const period = 2000;
 
-//  useEffect(() => {
-//    let ticker = setInterval(() => {
-//     tick();
-//    },delta)
+ useEffect(() => {
+   let ticker = setInterval(() => {
+    tick();
+   },delta)
 
-//    return () => {clearInterval(ticker)};
-//  }, [text])
+   return () => {clearInterval(ticker)};
+ }, [text])
 
-//  const tick = () => {
-//   let i = loopNum % toRotate.length;
-//   let fullText = toRotate[i];
-//   let updateText = isDeleting ? fullText.substring(0,text.length - 1) : fullText.substring(0,text.length + 1); 
+ const tick = () => {
+  let i = loopNum % toRotate.length;
+  let fullText = toRotate[i];
+  let updateText = isDeleting ? fullText.substring(0,text.length - 1) : fullText.substring(0,text.length + 1); 
  
-//   setText(updateText);
+  setText(updateText);
 
-//   if(isDeleting){
-//     setDelta(prevDelta => prevDelta /2)
-//   }
+  if(isDeleting){
+    setDelta(prevDelta => prevDelta /2)
+  }
 
-//   if(!isDeleting && updateText === fullText){
-//     setIsDeleting(true);
-//     setDelta(period);
-//   }
-//   else if(isDeleting && updateText === ''){
-//     setIsDeleting(false);
-//     setLoopNum(loopNum + 1);
-//     setDelta(500);
-//   }
+  if(!isDeleting && updateText === fullText){
+    setIsDeleting(true);
+    setDelta(period);
+  }
+  else if(isDeleting && updateText === ''){
+    setIsDeleting(false);
+    setLoopNum(loopNum + 1);
+    setDelta(500);
+  }
 
 
-// }
+}
 
 
 const downloadCV = () => {
